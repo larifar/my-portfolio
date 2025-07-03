46e819b99122461888e3b2faa1d21b113ab2b8e8
Larissa Faria <lari.f4ria@gmail.com>
2025-07-03 10:52:19 -0300

    create routes and button-navigation logic

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { AboutMeComponent } from './pages/about-me/about-me.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {path:'', redirectTo: 'aboutme', pathMatch: 'full'},
+  {path:'aboutme', component: AboutMeComponent},
+  {path:'projects', component: ProjectsComponent},
+  {path:'skills', component: SkillsComponent},
+  {path:'contact', component: ContactComponent},
+];
--- src/app/components/button-navigation/button-navigation.component.ts
+++ src/app/components/button-navigation/button-navigation.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-button-navigation',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive],
   templateUrl: './button-navigation.component.html',
   styleUrl: './button-navigation.component.css'
 })
 export class ButtonNavigationComponent {
-  @Input() page: String = "";
+  @Input() page: string = "";
+  @Input() to!: string;
 
 }
